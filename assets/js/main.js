1d318f6270b1c79f0b382f0b1b9a273f54e946aa
ramonsancheez <rsanchezabadia@cifpfbmoll.eu>
2023-07-06 22:29:27 +0200

    desktop home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,29 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-  var splide = new Splide( '.splide', {
+  var splide = new Splide( '#splide1', {
     type   : 'loop',
-    // autoWidth: true,
     gap: "5em",
     focus: 'center',
     perPage: 3,
+    autoplay: true,
+    interval: 2500,
+    arrows: false,
+    breakpoints: {
+      767: {
+        perPage: 1,
+      }
+    }
 
   } );
-  
   splide.mount();
+
+  var splide2 = new Splide('#splide2', {
+    type: 'loop',
+    perPage: 1,
+    pagination: false,
+  });
+  
+  splide2.mount();
 
   const menuTrigger = document.getElementById('menu-trigger');
   menuTrigger.addEventListener('click', function() {
